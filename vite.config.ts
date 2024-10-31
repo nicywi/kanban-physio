5278dba328d2c5d54fbc8d76cb022c89f5a9438e
Nikola Cywinska <nicywi@gmail.com>
2024-10-31 18:41:41 +0100

    hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -13,6 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: '/kanban-physio/',
   test: {
     globals: true,
     environment: 'jsdom',
